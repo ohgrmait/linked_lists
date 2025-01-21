f7d9b0766bbb19a970e1be325edcdaf6433e0bd0
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2025-01-21 19:47:17 +0530

    Add the logic for the append and to_s methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -5,4 +5,27 @@ class LinkedList # rubocop:disable Style/Documentation
     @head = nil
     @tail = nil
   end
+
+  def append(value)
+    # adds a new node containing value to the end of the list
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+    else
+      @tail.next_node = new_node
+    end
+    @tail = new_node
+  end
+
+  def to_s
+    # represent your LinkedList objects as strings, so you can
+    # print them out and preview them in the console. The format
+    # should be: ( value ) -> ( value ) -> ( value ) -> nil
+    tmp = @head
+    until tmp.nil?
+      print "( #{tmp.value} ) -> "
+      tmp = tmp.next_node
+    end
+    'nil' if tmp.nil?
+  end
 end
