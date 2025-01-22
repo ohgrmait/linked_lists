a1a8d494a65d3d5898e41ea260100883cc2edfa4
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2025-01-22 10:28:43 +0530

    Test out the size method

--- main.rb
+++ main.rb
@@ -7,16 +7,16 @@ list = LinkedList.new
 
 # puts 'Testing out the #append and #to_s methods...'
 
-# list.append('dog')
-# list.append('cat')
-# list.append('parrot')
-# list.append('hamster')
-# list.append('snake')
-# list.append('turtle')
+list.append('dog')
+list.append('cat')
+list.append('parrot')
+list.append('hamster')
+list.append('snake')
+list.append('turtle')
 
 # puts list
 
-puts 'Testing out the #prepend method...'
+# puts 'Testing out the #prepend method...'
 
 list.prepend('dog')
 list.prepend('cat')
@@ -25,4 +25,8 @@ list.prepend('hamster')
 list.prepend('snake')
 list.prepend('turtle')
 
-puts list
+# puts list
+
+puts 'Testing out the #size method...'
+
+puts list.size
