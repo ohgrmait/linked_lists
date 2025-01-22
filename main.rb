fd32897d8c999da49ea6e835154df9dc68d3df86
Argho Maitra <187108030+ohgrmait@users.noreply.github.com>
2025-01-22 10:24:18 +0530

    Test out the prepend method

--- main.rb
+++ main.rb
@@ -5,13 +5,24 @@ require_relative 'lib/node'
 
 list = LinkedList.new
 
-puts 'Testing out the #append and #to_s methods...'
+# puts 'Testing out the #append and #to_s methods...'
 
-list.append('dog')
-list.append('cat')
-list.append('parrot')
-list.append('hamster')
-list.append('snake')
-list.append('turtle')
+# list.append('dog')
+# list.append('cat')
+# list.append('parrot')
+# list.append('hamster')
+# list.append('snake')
+# list.append('turtle')
+
+# puts list
+
+puts 'Testing out the #prepend method...'
+
+list.prepend('dog')
+list.prepend('cat')
+list.prepend('parrot')
+list.prepend('hamster')
+list.prepend('snake')
+list.prepend('turtle')
 
 puts list
